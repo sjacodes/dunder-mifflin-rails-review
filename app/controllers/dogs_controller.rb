d424b48a448f60e6ecc9a269d605b70f5be38dc8
github username <github email address>
2019-02-26 13:45:51 +0000

    solution complete for updated lab

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,24 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+    
+    def dogs_up_for_adoption
+        @dogs = Dog.all.select {|dog| dog.employees.empty?}
+    end
+
+    def adopt_a_dog_form
+        @dog = Dog.find(params[:id])
+    end
+
+    def adopt
+        @dog = Dog.find(params[:id])
+        employee = Employee.find(params[:new_employee_id])
+        @dog.employees << employee
+        redirect_to dogs_up_for_adoption_path
+    end
+   
+   
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+  get '/dogs_up_for_adoption', to: 'dogs#dogs_up_for_adoption', as: 'dogs_up_for_adoption'
+  get '/adopt/:id', to: 'dogs#adopt_a_dog_form', as: 'adopt_dog_form'
+  post '/adopt/:id', to: 'dogs#adopt', as: 'adopt'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
